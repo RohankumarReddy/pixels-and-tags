5c8f0f724edd87eb3679cf21630c38146939dc70
Nagaruru Rohankumar Reddy <150910334+RohankumarReddy@users.noreply.github.com>
2025-07-28 23:33:48 +0530

    Add express files to 06-backend

---
+++ 05-backend/01-node-and-express/Express/Express Server/index.js
@@ -0,0 +1,14 @@
+
+ import express from 'express'
+ const app = express()
+ const port = 3000
+app.get("/",(req,res)=>{
+  res.send("<h1>hello,world</h1>")
+})
+app.get("/about",(req,res)=>{
+  res.send("<h1>hello,there</h1>")
+})
+
+ app.listen(port,()=>{
+  console.log(`Server is running ${port}`)
+ })
