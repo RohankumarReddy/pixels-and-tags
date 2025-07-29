3bdf2ce8d6729f3d7d68c37a29c71b50d68a9703
Nagaruru Rohankumar Reddy <150910334+RohankumarReddy@users.noreply.github.com>
2025-07-30 00:10:18 +0530

    Add middleware and secrels project to backend/node-and-express

---
+++ 05-backend/01-node-and-express/Express/Middleware/index4.js
@@ -0,0 +1,31 @@
+import express from "express";
+import { dirname } from "path";
+import { fileURLToPath } from "url";
+import bodyParser from "body-parser";
+const __dirname = dirname(fileURLToPath(import.meta.url));
+
+
+const app = express();
+const port = 3000;
+app.use(bodyParser.urlencoded({extended:true}));
+
+app.get("/", (req, res) => {
+ 
+  res.sendFile(__dirname + "/public/index.html");
+});
+
+function bandname(req,res,next){
+  console.log(req.body["street"]+req.body["pet"]);
+  next();
+}
+app.use(bandname);
+app.post('/submit',bandname,(req,res)=>{
+  const bandName = req.body["street"] + req.body["pet"];
+   res.send(`<h1>Your Band Name is: ${bandName}</h1>`);
+})
+
+
+app.listen(port, () => {
+  console.log(`Listening on port ${port}`);
+});
+
