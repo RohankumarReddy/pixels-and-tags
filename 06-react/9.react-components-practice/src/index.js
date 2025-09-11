e35909afdcf9eb6ae93d49c8a6eedc302de66ba2
Nagaruru Rohankumar Reddy <150910334+RohankumarReddy@users.noreply.github.com>
2025-09-11 21:10:03 +0530

    Update react folder (module 9)

---
+++ 06-react/9.react-components-practice/src/index.js
@@ -0,0 +1,6 @@
+import React from "react";
+import ReactDOM from "react-dom";
+import "./../public/styles.css";
+import Heading from "../public/Heading";
+
+ReactDOM.render(<Heading />, document.getElementById("root"));
